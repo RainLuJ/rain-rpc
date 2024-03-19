c2a2e9a49a7c37bd3ea59e927b17bac75c6d8d47
RainLuJ <lujun61@outlook.com>
2024-03-19 23:24:10 +0800

    使用SPI机制实现多种序列化配置

---
+++ rain-rpc-core/src/main/java/com/rainlu/rpc/core/registry/LocalRegistry.java
@@ -0,0 +1,48 @@
+package com.rainlu.rpc.core.registry;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 本地注册中心
+ */
+public class LocalRegistry {
+
+    /**
+     * 注册信息存储
+     * - key：服务全限定名称
+     * - value：服务的实现类class
+     *
+     * 目标是根据服务名称来获取到服务实现类，然后通过反射的方式调用方法
+     */
+    private static final Map<String, Class<?>> map = new ConcurrentHashMap<>();
+
+    /**
+     * 注册服务
+     *
+     * @param serviceName
+     * @param implClass
+     */
+    public static void register(String serviceName, Class<?> implClass) {
+        map.put(serviceName, implClass);
+    }
+
+    /**
+     * 获取服务
+     *
+     * @param serviceName
+     * @return
+     */
+    public static Class<?> get(String serviceName) {
+        return map.get(serviceName);
+    }
+
+    /**
+     * 删除服务
+     *
+     * @param serviceName
+     */
+    public static void remove(String serviceName) {
+        map.remove(serviceName);
+    }
+}
---
+++ rain-rpc-core/src/main/java/com/rainlu/rpc/core/serializer/SerializerKeys.java
@@ -0,0 +1,13 @@
+package com.rainlu.rpc.core.serializer;
+
+/**
+ * 系统自带的序列化器键名常量
+ */
+public interface SerializerKeys {
+
+    String JDK = "jdk";
+    String JSON = "json";
+    String KRYO = "kryo";
+    String HESSIAN = "hessian";
+
+}
